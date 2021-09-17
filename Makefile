404a3e8405d6621587d2aead12341f9214898bed
ThorsteinSkaug <thorstein.skaug@fys.uio.no>
2021-09-17 12:01:41 +0200

    second-push

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 all: Compile Link Execute
 
 Compile:
-	g++ -c -std=c++11 project2_3.cpp -larmadillo
+	g++ -c -std=c++11 project2_4.cpp -larmadillo
 
 Link:
-	g++ -std=c++11 project2_3.o -o project2_3.exe  -larmadillo 
+	g++ -std=c++11 project2_4.o -o project2_4.exe  -larmadillo
 
 Execute:
-	./project2_3.exe
+	./project2_4.exe
--- project2_4.cpp
+++ project2_4.cpp
@@ -1,24 +1,31 @@
+#include <armadillo>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <math.h>
+#include <vector>
+#include <chrono>
+#include <cmath>
 
-
-double max_offdiag_symmetric(arma::mat A, int& k, int &l){
+double max_offdiag_symmetric(arma::mat A, int& k, int &l, int size){
   double max_val = 0;
-  int mat_size = len(A);
-  for(i=1; i<mat_size; i++){
-    for(j=i+1; j<mat_size; j++){
+  int mat_size = size;//len(A);
+  for(int i=0; i<mat_size-1; i++){
+    for(int j=i+1; j<mat_size; j++){
       if(abs(A(i,j)) > max_val){
-        max_val = abs(A(i,j);
+        max_val = abs(A(i,j));
         k = i;
         l = j;
       }
     }
   }
-  return max_val
+  return max_val;
 }
 
 int main()
 {
     arma::mat A (4,4, arma::fill::zeros);
-    for(i=0;i<4;i++){
+    for(int i=0;i<4;i++){
       A(i,i) = 1;
     }
     A(3,0) = 0.5;
@@ -26,7 +33,9 @@ int main()
     A(1,2) = -0.7;
     A(2,1) = -0.7;
 
-    double max_A = max_offdiag_symmetric(A, int k, int l);
+    int k;
+    int l;
+    double max_A = max_offdiag_symmetric(A, k, l, 4);
 
-    std::cout << max_A;
+    std::cout << max_A << '\n';
 }
